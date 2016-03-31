f903775c9c9e9189ad23a110232c4b47d318baeb
Manu Phatak <bionikspoon@gmail.com>
2016-03-31 02:57:46 -0500

    hacks

--- common/models/review.js
+++ common/models/review.js
@@ -1,3 +1,11 @@
-module.exports = function(Review) {
+module.exports = function (Review) {
+  Review.beforeRemote('create', beforeCreate);
 
+  function beforeCreate(context, user, next) {
+    var req = context.req;
+    req.body.date = Date.now();
+    req.body.publisherId = req.accessToken.userId;
+
+    next();
+  }
 };
